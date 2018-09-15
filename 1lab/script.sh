ffaeea6c2106b960e3419443dcf36e78c31450cf
hegemonies <hegemonies@yandex.ru>
2018-09-15 16:06:06 +0700

    Fix 1lab

--- 1lab/script.sh
+++ 1lab/script.sh
@@ -3,30 +3,43 @@ echo "Дата: "$(date)
 echo "Имя учетной записи: "$USER
 echo "Доменное имя ПК: "$(hostname)
 
-cpus="$(lscpu | grep 'CPU(s)')"
-lscpu | grep "Имя модели\|Архитектура\|CPU MHz\|Thread(s) per core" 
-printf '%b' "Количество ядер -    "
-echo $cpus | awk '{print $2}'
+#cpus="$(lscpu | grep 'CPU(s)')"
+#lscpu | grep "Имя модели\|Архитектура\|CPU MHz\|Thread(s) per core" 
+#printf '%b' "Количество ядер -    "
+#echo $cpus | awk '{print $2}'
+
+echo "Процессор:"
+printf '  *  %s %b\n' "Модель - $(lscpu | grep 'Model name' | awk '{print $3, $4, $5}')"
+printf '  *  %s ' "Архитектура - "
+arch="$(lscpu | grep 'Архитектура')"
+echo ${arch:12}
+printf '  *  %s %b\n' "Тактовая частота - $(lscpu | grep 'Model name' | awk '{print $8}')"
+printf '  *  %s %b\n' "Количество ядер - $(lscpu | grep 'CPU(s)' | head -n1 | awk '{print $2}')"
+printf '  *  %s ' "Количество потоков на одно ядро - "
+arch="$(lscpu | grep 'Потоков на ядро')"
+echo ${arch:16}
+
 printf '\n%b\n' "Оперативная память:"
-cat /proc/meminfo | grep "MemTotal"
-cat /proc/meminfo | grep "MemFree"
+printf '  *  %s %b\n' "Всего - $(free -h | head -n2 | tail -n1 | awk '{print $2}')"
+printf '  *  %s %b\n' "Доступно - $(free -h | head -n2 | tail -n1 | awk '{print $4}')"
+
 printf '\n%b\n' 'Жесткий диск'
 DF="$(df -h | grep /dev/sda1)"
-printf '%b' "  Файловая система - "
+printf '  *  %b' "  Файловая система - "
 echo $DF | awk '{print $1}'
-printf '%b' "  Всего - "
+printf '  *  %b' "  Всего - "
 echo $DF | awk '{print $2}'
-printf '%b' "  Доступно - "
+printf '  *  %b' "  Доступно - "
 echo $DF | awk '{print $4}'
-printf '%b' "  Смонтировано - "
+printf '  *  %b' "  Смонтировано - "
 echo $DF | awk '{print $6}'
 
 SWAP="$(swapon | tail -n1)"
-printf '%b' '  Swap - '
+printf '  *  %b' '  Swap - '
 echo $SWAP | awk '{print $1}'
-printf '%b' '  Swap всего - '
+printf '  *  %b' '  Swap всего - '
 echo $SWAP | awk '{print $3}'
-printf '%b' '  Swap доступно - '
+printf '  *  %b' '  Swap доступно - '
 echo $SWAP | awk '{print $4}'
 
 
@@ -46,43 +59,51 @@ if [[ $COUNT_OF_LINE_ETH > 18 ]]; then
 	let "COUNT_OF_ETH_CON += 1"
 fi
 
-echo "Количество сетевых интерфейсов - "$COUNT_OF_ETH_CON
+echo "  *  Количество сетевых интерфейсов - "$COUNT_OF_ETH_CON
+echo "№ Имя сетевого интерфейса MAC адрес          IP адрес        Скорость соединения"
 
 let "j = 1"
+
 for (( i=0; i < $COUNT_OF_ETH_CON; i++ ))
 do
-
-	printf '%b' 'Name: '
+	echo "--------------------------------------------------------------------------------"
+	printf '%s ' $j
+	#printf '%b' 'Name: '
 	let "need_line = ($i * 9) + 1"
 	name="$(ifconfig | awk '{print $1}' | head -n$need_line | tail -n1)"
-	echo $name
+	printf '%s\t\t  ' $name
 	
 	inet="$(ifconfig | grep 'inet' | awk '{print $2}')"
-	mac="$(ifconfig | grep 'ether' | awk '{print $2}')"
+	#mac="$(ifconfig | grep 'ether' | awk '{print $2}')"
+	mac="$(ifconfig | grep 'HWaddr' | awk '{print $5}')"
 	
-	if [[ $name != "lo:" ]]; then
-		printf '%b' 'Ip: '
-		let "need_str = ($i * 2) + 1"
-		echo $inet | awk -v need_str="$need_str" '{print $need_str}'
-		
-		printf '%b' 'Mac: '
-		echo $mac | awk -v j="$j" '{print $j}'
+	if [[ $name != "lo" ]]; then
+		#printf '%b' 'Mac: '
+		echo -n "$(echo $mac | awk -v j="$j" '{print $j}')  "
+		#printf '%b ' "$(ifconfig | grep 'HWaddr' | awk '{print $5}' | awk -v j="$j" '{print $j}')"
 		let "j += 1"
-	
-		ifconfig $name up
-		printf '%b' 'Download: '
-		#speedtest="$(wget -q -O - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python | tail -n3)"
-		echo $speedtest | awk '{print $2, $3}'
-		printf '%b' 'Upload: '
-		echo $speedtest | awk '{print $8, $9}'
-	else 
-		printf '%b' 'Ip: '
+
+		#printf '%b' 'Ip: '
 		let "need_str = ($i * 2) + 1"
-		echo $inet | awk -v need_str="$need_str" '{print $need_str}'
-		printf '%b\n' 'Mac: -'
-		echo 'Download: local loop :('
-		echo 'Upload: local loop :('
+		#echo $inet | awk -v need_str="$need_str" '{print $need_str}'
+		#printf '%b\n' "$(ifconfig | grep 'inet' | awk '{print $2}' | awk -v need_str="$need_str" '{print $need_str}')"
+		echo -n "$(echo $inet | awk -v need_str="$need_str" '{print $need_str}')  "
+	
+		#ifconfig $name up >> /dev/random
+		speedtest="$(wget -q -O - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python | tail -n3)"
+		printf '%b' 'Down: '
+		echo "$(echo $speedtest | awk '{print $2, $3}')"
+		printf '\t\t\t\t\t\t\t     %b' 'Up:   '
+		echo "$(echo $speedtest | awk '{print $8, $9}') "
+	else 
+		#printf '%b' 'Ip: '
+		let "need_str = ($i * 2) + 1"
+		#echo $inet | awk -v need_str="$need_str" '{print $need_str}'
+		printf '\t  %b \t\t     ' '-' 
+		echo -n "$(echo $inet | awk -v need_str="$need_str" '{print $need_str}')  "
+		echo 'Download :( '
+		printf '\t\t\t\t\t\t\t     '
+		echo 'Upload :('
 	fi
 
-	echo "----------"
 done
--- 1lab/test.sh
+++
@@ -1,8 +0,0 @@
-#tmp="$(swapon | tail -n1)"
-#echo ${tmp:0:30} | sed 's/[ *]/\t\t/g' 
-#cpus="$(lscpu | grep 'CPU(s)')"
-#echo $cpus | awk '{print $2}'
-
-
-name="$(ifconfig | awk '{print $1}' | head -n26)"
-printf '%b' $name
